82fcd758275c9c61fbf9ea684750d2e6270c71ad
Anderson <anderson>
2015-03-12 22:22:34 -0300

    testando

--- app/src/main/java/com/example/rungroup/rungroup/MainActivity.java
+++ app/src/main/java/com/example/rungroup/rungroup/MainActivity.java
@@ -21,6 +21,7 @@ public class MainActivity extends ActionBarActivity {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
+    //Testando GitHub
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
